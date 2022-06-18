49d12c0c2bf2d77db43fb7767e2439365d6912e9
Angelica <Angelica137@users.noreply.github.com>
2022-06-18 19:39:07 +0100

    test solution returns 1, 4, red

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -3,3 +3,7 @@ from scripts.solution import solution
 
 def test_solution_1():
     assert solution([1, 2, 3], 0) == []
+
+
+def test_solution_2():
+    assert solution([1, 2, 2, 3, 3, 3, 4, 5, 5], 1) == 1, 4
