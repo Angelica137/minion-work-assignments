5bd16fdc5dc5da16b820e78e7bde28620187f8dd
Angelica <Angelica137@users.noreply.github.com>
2022-06-18 19:42:18 +0100

    remove print statments

--- scripts/solution.py
+++ scripts/solution.py
@@ -6,11 +6,7 @@ def solution(data: list, n: int) -> list:
     """
     for item in data:
         count = data.count(item)
-        print(count)
         if count > n:
             data = [x for x in data if x != item]
         item += 1
     return data
-
-
-print(solution([1, 2, 2, 3, 3, 3, 4, 5, 5], 1))
