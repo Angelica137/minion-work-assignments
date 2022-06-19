f2d8b51929e52bbaf412f720f1502d2c198e5b70
Angelica <Angelica137@users.noreply.github.com>
2022-06-19 19:18:48 +0100

    try to skip lists of more than 99 items, red

--- scripts/solution.py
+++ scripts/solution.py
@@ -1,10 +1,26 @@
+import random
+
+
 def solution(data: list, n: int) -> list:
     """
-    Removes the items that are greater than :param: n
+    Removes the items that repeat more times than :param: n
     from :param: data
     :returns: the updated list
     """
-    for item in data:
-        if data.count(item) > n:
-            data = [x for x in data if x != item]
-    return data
+    if len(data) >= 10:
+        return "No"
+    if len(data) < 5:
+        for item in data:
+            if data.count(item) > n:
+                data = [x for x in data if x != item]
+        return data
+
+
+data = []
+for i in range(0, 100):
+    n = random.randint(1, 100)
+    data.append(n)
+
+print(len(data))
+
+print(solution([data], 1))
--- tests/test_solution.py
+++ tests/test_solution.py
@@ -1,5 +1,7 @@
 from scripts.solution import solution
 
+import random
+
 
 def test_solution_1():
     assert solution([1, 2, 3], 0) == []
@@ -11,3 +13,19 @@ def test_solution_2():
 
 def test_solution_3():
     assert solution([5, 10, 15, 10, 7], 1) == [5, 15, 7]
+
+
+def test_solution_4():
+    assert solution([1, 2, 3], 6) == [1, 2, 3]
+
+
+def test_solution_5():
+    assert solution([], 6) == []
+
+
+def test_more_than_100():
+    data = []
+    for i in range(0, 100):
+        n = random.randint(1, 100)
+        data.append(n)
+    assert solution([data], 1) == None
